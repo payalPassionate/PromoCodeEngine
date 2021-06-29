25fa3ce5e29bbb77f9801f544c2136bdcf3dee95
Payal41 <das.payal1989@gmail.com>
2021-06-29 20:22:09 +0100

    Added Mock dats to test extra scenario of combined promo generic behaviour.

--- src/main/java/com/promoengine/app/util/MockData.java
+++ src/main/java/com/promoengine/app/util/MockData.java
@@ -10,6 +10,10 @@ import com.promoengine.app.model.PromoFixed;
 
 public class MockData {
 
+	/**
+	 * Scenario A - Test set Up - Check logic when no promo applied.
+	 * @return
+	 */
 	public static List<CartItemDetails> buildCartItemDetailsForScenarioA() {
 		CartItemDetails ci = new CartItemDetails();
 		ci.setSkuId("A");
@@ -29,6 +33,10 @@ public class MockData {
 		return Arrays.asList(ci, ci1, ci2);
 	}
 	
+	/**
+	 * Scenario B - Test set Up - Check fixed promo logic.
+	 * @return
+	 */
 	public static List<CartItemDetails> buildCartItemDetailsForScenarioB() {
 		CartItemDetails ci = new CartItemDetails();
 		ci.setSkuId("A");
@@ -48,7 +56,10 @@ public class MockData {
 		return Arrays.asList(ci, ci1, ci2);
 	}
 	
-
+	/**
+	 * Scenario C - Test set Up - Check mix of combine and fixed promo.
+	 * @return
+	 */
 	public static List<CartItemDetails> buildCartItemDetailsForScenarioC() {
 		CartItemDetails ci = new CartItemDetails();
 		ci.setSkuId("A");
@@ -71,6 +82,47 @@ public class MockData {
 		ci3.setPeritemprice(15);
 
 		return Arrays.asList(ci, ci1, ci2, ci3);
+	}
+	
+	/**
+	 * Scenario D - Test set Up - Check mix of combine and fixed promo.
+	 * Added extra scenario to test Flexi Combine Promo logic.
+	 * Item E and F has been added to cart which is eligble for Combine Promo.
+	 * The below scenario is enhancement of Scenario C.
+	 * @return
+	 */
+	public static List<CartItemDetails> buildCartItemDetailsForScenarioDtoChcekMultiCombinepromo() {
+		CartItemDetails ci = new CartItemDetails();
+		ci.setSkuId("A");
+		ci.setQuantity(3);
+		ci.setPeritemprice(50);
+
+		CartItemDetails ci1 = new CartItemDetails();
+		ci1.setSkuId("B");
+		ci1.setQuantity(5);
+		ci1.setPeritemprice(30);
+
+		CartItemDetails ci2 = new CartItemDetails();
+		ci2.setSkuId("C");
+		ci2.setQuantity(1);
+		ci2.setPeritemprice(20);
+		
+		CartItemDetails ci3 = new CartItemDetails();
+		ci3.setSkuId("D");
+		ci3.setQuantity(1);
+		ci3.setPeritemprice(15);
+		
+		CartItemDetails ci4 = new CartItemDetails();
+		ci4.setSkuId("E");
+		ci4.setQuantity(1);
+		ci4.setPeritemprice(40);
+		
+		CartItemDetails ci5 = new CartItemDetails();
+		ci5.setSkuId("F");
+		ci5.setQuantity(1);
+		ci5.setPeritemprice(30);
+
+		return Arrays.asList(ci, ci1, ci2, ci3, ci4, ci5);
 	}
 
 	public static ActivePromotion buildActivePromotion() {
@@ -100,8 +152,14 @@ public class MockData {
 		pc.setSkuid1("C");
 		pc.setSkuid2("D");
 		pc.setDiscountedprice(30);
+		
+		PromoCombined pc1 = new PromoCombined();
+		pc1.setSkuid1("E");
+		pc1.setSkuid2("F");
+		pc1.setDiscountedprice(50);
 
-		return Arrays.asList(pc);
+
+		return Arrays.asList(pc, pc1);
 
 	}
 }
