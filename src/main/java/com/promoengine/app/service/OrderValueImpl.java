dce9638f2bab3b27cffac9ae46a863d5d71c6eef
Payal41 <das.payal1989@gmail.com>
2021-06-29 18:21:23 +0100

    Adding logic for price calculation without any Promo - satisfies scenario A.

--- src/main/java/com/promoengine/app/service/OrderValueImpl.java
+++ src/main/java/com/promoengine/app/service/OrderValueImpl.java
@@ -2,13 +2,26 @@ package com.promoengine.app.service;
 
 import java.util.List;
 
+import com.promoengine.app.model.ActivePromotion;
 import com.promoengine.app.model.CartItemDetails;
+import com.promoengine.app.model.PromoCombined;
+import com.promoengine.app.model.PromoFixed;
+import com.promoengine.app.util.MockData;
 
-public class OrderValueImpl implements OrderValue{
+public class OrderValueImpl implements OrderValue {
 
 	public double calculatePrice(List<CartItemDetails> ci) {
-		// TODO Auto-generated method stub
-		return 0;
+		ActivePromotion ap = MockData.buildActivePromotion();
+		List<PromoFixed> pf = ap.getPf();
+		List<PromoCombined> pc = ap.getPc();
+		
+		double finalPrice = 0;
+		
+		for(CartItemDetails item : ci) {
+			finalPrice = finalPrice + item.getQuantity() * item.getPeritemprice();
+		}
+
+		return finalPrice;
 	}
 
 }
